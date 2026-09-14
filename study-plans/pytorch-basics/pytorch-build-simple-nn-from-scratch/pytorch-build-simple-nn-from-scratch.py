ada06bf871940a58b47271a43c70441b44c987e7
Felipe Mulato <87508112+FelipeMulato@users.noreply.github.com>
2026-09-14 15:26:08 -0300

    Solve pytorch-build-simple-nn-from-scratch

--- study-plans/pytorch-basics/pytorch-build-simple-nn-from-scratch/pytorch-build-simple-nn-from-scratch.py
+++ study-plans/pytorch-basics/pytorch-build-simple-nn-from-scratch/pytorch-build-simple-nn-from-scratch.py
@@ -7,11 +7,10 @@ class SimpleNet(nn.Module):
     """
 
     def __init__(self, in_features, hidden_size, out_features):
-        super().__init__()
-        self.w1 = nn.Linear(in_features,hidden_size)
-        self.w2 = nn.Linear(hidden_size,out_features)
-        self.relu =nn.ReLU()
+         super().__init__()
+         self.w1 = nn.Linear(in_features,hidden_size)
+         self.w2 = nn.Linear(hidden_size,out_features)
+         self.relu = nn.ReLU()
 
     def forward(self, x):
         return self.w2(self.relu(self.w1(x)))
-        
